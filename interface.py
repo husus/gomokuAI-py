5f5311ca03ded866e7d7f46023abf16791311986
Susan <Susan@MacBook-di-Susan.local>
2022-06-03 23:01:32 +0200

    Added menu buttons function

--- interface.py
+++ interface.py
@@ -2,32 +2,31 @@ import pygame
 import os
 from AI import *
 from gomoku import *
+from button import *
 
 SIZE = 540 #size of the board image
 PIECE = 32 #size of the single pieces
 N = 15
 MARGIN = 23
-# GRID = 35
 GRID = (SIZE - 2 * MARGIN) / (N-1)
-# 35x35 size of squares in the board
-
-'''
-(23,23) start of internal square --> (7,7)
-(517,23) top right end of internal square --> (533,7)
-(23,517) bottom left start of internal square --> (7,533)
-(517,517) bottom right end of internal square --> (533,533)
-'''
-
 
 SCREEN = pygame.display.set_mode((SIZE, SIZE))
 pygame.display.set_caption('Play Gomoku!')
 
 BG = pygame.image.load(os.path.join("assets", 'board.jpg')).convert() #set board image as bg
 BLACK_PIECE = pygame.image.load(os.path.join("assets", 'black_piece.png')).convert_alpha()
-# BLACK_PIECE = pygame.transform.scale(BLACK_PIECE,(35,35))
-
 WHITE_PIECE = pygame.image.load(os.path.join("assets", 'white_piece.png')).convert_alpha()
-# WHITE_PIECE = pygame.transform.scale(WHITE_PIECE,(35,35))
+
+# Elements for the starting menu interface
+MENU_BOARD = pygame.image.load(os.path.join("assets", "menu_board.png")).convert_alpha()
+MENU_BOARD = pygame.transform.scale(MENU_BOARD, (350, 100))
+BUTTON_SURF = pygame.image.load(os.path.join("assets", "button.png")).convert_alpha()
+BUTTON_SURF = pygame.transform.scale(BUTTON_SURF, (110, 60))
+# Setting the font of the text in the starting menu
+MENU_FONT = pygame.font.SysFont("arial", 22)
+
+
+
 
 # FPS = 60 #how many frames per second to update the window
 
@@ -70,7 +69,6 @@ WHITE_PIECE = pygame.image.load(os.path.join("assets", 'white_piece.png')).conve
     # pygame.display.update()
 
 def pixel_conversion(list_points, target):
-
     # point of the list from where start the search
     index = int((len(list_points)-1)//2) 
 
@@ -123,6 +121,18 @@ def create_mapping():
     
     return pos_mapping
 
+def draw_menu(menu_board, menu_font, scale): 
+    menu_board = pygame.transform.scale(menu_board, scale)
+    menu_board_rect = menu_board.get_rect(center = SCREEN.get_rect().center)
+    menu_text = menu_font.render('CHOOSE YOUR COLOR: ', True, 'white')
+    menu_board.blit(menu_text, (50,25))
+    SCREEN.blit(menu_board, menu_board_rect)
+
+def draw_button(button):
+    button.update()
+    button.change_color(pygame.mouse.get_pos())
+    pygame.display.update()
+
 def draw_piece(state, mapping, i, j):
     # (x, y) = pygame.mouse.get_pos()
 
@@ -140,14 +150,14 @@ def draw_piece(state, mapping, i, j):
     
 def draw_result(winner, tie=False):
     text = ' The winner is: '
-    font = pygame.font.SysFont('Times New Roman', 35)
+    text_font = pygame.font.SysFont('Times New Roman', 35)
 
-    render_text = font.render(str.upper(text), True, (255,255,255), (0,0,0))
+    render_text = text_font.render(str.upper(text), True, (255,255,255), (0,0,0))
     size1 = render_text.get_size()
     x1 = SIZE//2 - size1[0]//2
     y1 = SIZE//2 - size1[1]
 
-    render_winner = font.render(str.upper(winner), True, (255,255,255), (0,0,0))
+    render_winner = text_font.render(str.upper(winner), True, (255,255,255), (0,0,0))
     size2 = render_winner.get_size()
     x2 = SIZE//2 - size2[0]//2
     y2 = (SIZE//2 - size2[1]) + size1[1]
@@ -165,14 +175,22 @@ def draw_result(winner, tie=False):
     pygame.display.update()
 
 def main():
-    pygame.init()
-    # clock = pygame.time.Clock()
     run = True
-    SCREEN.blit(BG, (0,0))
-    pygame.display.update()
     state = 1
     pos_mapping = create_mapping()
     ai = GomokuAI(3)
+
+    pygame.init()
+    # clock = pygame.time.Clock()
+    SCREEN.blit(BG, (0,0))
+    draw_menu(MENU_BOARD, MENU_FONT, (350,100))
+
+    button_black = Button(button_surf, 200, 300, "BLACK", MENU_FONT)
+    button_white = Button(button_surf, 340, 300, "WHITE", MENU_FONT)   
+    draw_button(button_black) 
+    draw_button(button_white)
+    
+    pygame.display.update()
 
     while run:
         # clock.tick(FPS) #to control the speed of while loop, never go over that speed
@@ -181,9 +199,19 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
+                button_white.check_mouse_pos(pos)
+                button_black.check_mouse_pos(pos)
+        draw_button(button_black) 
+        draw_button(button_white)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
                 if pygame.mouse.get_pressed()[0]:
                     pos = pygame.mouse.get_pos()
-                    print(pos)
+                    
+
                     (i, j) = pos_pixel2map(pos[0], pos[1])
                     print((i,j))
         
