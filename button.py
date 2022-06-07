ff42998d2238ccd35e676c9d0b7ec8c37f5c1a00
Susan <Susan@MacBook-di-Susan.local>
2022-06-08 00:38:09 +0200

    Update Button class

--- button.py
+++ button.py
@@ -1,7 +1,6 @@
 import pygame 
 
 pygame.init()
-screen = pygame.display.set_mode((540,540))
 
 class Button():
     def __init__(self, image, x_pos, y_pos, text_input, button_font):
@@ -14,23 +13,25 @@ class Button():
         self.text = self.button_font.render(self.text_input, True, "white")
         self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
-    def update(self):
-        screen.blit(self.image, self.rect)
-        screen.blit(self.text, self.text_rect)
+    def update(self, surface):
+        surface.blit(self.image, self.rect)
+        surface.blit(self.text, self.text_rect)
         
     def check_mouse_pos(self, position):
         return position[0] in range(self.rect.left, self.rect.right) and\
 			 position[1] in range(self.rect.top, self.rect.bottom)
 
     def change_color(self, position):
-        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-            self.text = self.button_font.render(self.text_input, True, 'yellow')#(92,64,51))
+        if position[0] in range(self.rect.left, self.rect.right) and\
+            position[1] in range(self.rect.top, self.rect.bottom):
+            self.text = self.button_font.render(self.text_input, True, 'yellow')
         else:
             self.text = self.button_font.render(self.text_input, True, "white")
             
-    def draw_button(self):
-        self.update()
+    def draw_button(self, surface):
+        self.update(surface)
         self.change_color(pygame.mouse.get_pos())
         pygame.display.update()
+        
 
 
