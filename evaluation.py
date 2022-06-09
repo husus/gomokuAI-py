2a9e1f5364157b68dc9fc00399688e7be81d459c
Susan <Susan@MacBook-di-Susan.local>
2022-06-10 00:22:18 +0200

    Update pattern evaluation

--- evaluation.py
+++ evaluation.py
@@ -6,66 +6,49 @@ def create_pattern_dict():
         # long_5
         patternDict[(x, x, x, x, x)] = 1000000 * x
         # live_4
-        patternDict[(0, x, x, x, x, 0)] = 1000 * x
+        patternDict[(0, x, x, x, x, 0)] = 50000 * x
         # go_4
-        patternDict[(0, x, x, x, x, y)] = 500 * x
-        patternDict[(y, x, x, x, x, 0)] = 500 * x
-        patternDict[(0, x, x, x, 0, x, 0)] = 500 * x
-        patternDict[(0, x, 0, x, x, x, 0)] = 500 * x
-        patternDict[(0, x, x, 0, x, x, 0)] = 500 * x
+        patternDict[(0, x, x, x, x, y)] = 10000 * x
+        patternDict[(y, x, x, x, x, 0)] = 10000 * x
+        patternDict[(0, x, x, x, 0, x, 0)] = 10000 * x
+        patternDict[(0, x, 0, x, x, x, 0)] = 10000 * x
+        patternDict[(0, x, x, 0, x, x, 0)] = 10000 * x
         # dead_4
-        patternDict[(y, x, x, x, x, y)] = -5 * x
+        patternDict[(y, x, x, x, x, y)] = -50 * x
         # live_3
-        patternDict[(0, x, x, x, 0)] = 200 * x
-        patternDict[(0, x, 0, x, x, 0)] = 200 * x
-        patternDict[(0, x, x, 0, x, 0)] = 200 * x
+        patternDict[(0, x, x, x, 0)] = 5000 * x
+        patternDict[(0, x, 0, x, x, 0)] = 5000 * x
+        patternDict[(0, x, x, 0, x, 0)] = 5000 * x
         # sleep_3
-        patternDict[(0, 0, x, x, x, y)] = 50 * x
-        patternDict[(y, x, x, x, 0, 0)] = 50 * x
-        patternDict[(0, x, 0, x, x, y)] = 50 * x
-        patternDict[(y, x, x, 0, x, 0)] = 50 * x
-        patternDict[(0, x, x, 0, x, y)] = 50 * x
-        patternDict[(y, x, 0, x, x, 0)] = 50 * x
-        patternDict[(x, 0, 0, x, x)] = 50 * x
-        patternDict[(x, x, 0, 0, x)] = 50 * x
-        patternDict[(x, 0, x, 0, x)] = 50 * x
-        patternDict[(y, 0, x, x, x, 0, y)] = 50 * x
+        patternDict[(0, 0, x, x, x, y)] = 500 * x
+        patternDict[(y, x, x, x, 0, 0)] = 500 * x
+        patternDict[(0, x, 0, x, x, y)] = 500 * x
+        patternDict[(y, x, x, 0, x, 0)] = 500 * x
+        patternDict[(0, x, x, 0, x, y)] = 500 * x
+        patternDict[(y, x, 0, x, x, 0)] = 500 * x
+        patternDict[(x, 0, 0, x, x)] = 500 * x
+        patternDict[(x, x, 0, 0, x)] = 500 * x
+        patternDict[(x, 0, x, 0, x)] = 500 * x
+        patternDict[(y, 0, x, x, x, 0, y)] = 500 * x
         # dead_3
-        patternDict[(y, x, x, x, y)] = -5 * x
+        patternDict[(y, x, x, x, y)] = -50 * x
         # live_2
-        patternDict[(0, 0, x, x, 0)] = 5 * x
-        patternDict[(0, x, x, 0, 0)] = 5 * x
-        patternDict[(0, x, 0, x, 0)] = 5 * x
-        patternDict[(0, x, 0, 0, x, 0)] = 5 * x
+        patternDict[(0, 0, x, x, 0)] = 100 * x
+        patternDict[(0, x, x, 0, 0)] = 100 * x
+        patternDict[(0, x, 0, x, 0)] = 100 * x
+        patternDict[(0, x, 0, 0, x, 0)] = 100 * x
         # sleep_2
-        patternDict[(0, 0, 0, x, x, y)] = 3 * x
-        patternDict[(y, x, x, 0, 0, 0)] = 3 * x
-        patternDict[(0, 0, x, 0, x, y)] = 3 * x
-        patternDict[(y, x, 0, x, 0, 0)] = 3 * x
-        patternDict[(0, x, 0, 0, x, y)] = 3 * x
-        patternDict[(y, x, 0, 0, x, 0)] = 3 * x
-        patternDict[(x, 0, 0, 0, x)] = 3 * x
-        patternDict[(y, 0, x, 0, x, 0, y)] = 3 * x
-        patternDict[(y, 0, x, x, 0, 0, y)] = 3 * x
-        patternDict[(y, 0, 0, x, x, 0, y)] = 3 * x
+        patternDict[(0, 0, 0, x, x, y)] = 50 * x
+        patternDict[(y, x, x, 0, 0, 0)] = 50 * x
+        patternDict[(0, 0, x, 0, x, y)] = 50 * x
+        patternDict[(y, x, 0, x, 0, 0)] = 50 * x
+        patternDict[(0, x, 0, 0, x, y)] = 50 * x
+        patternDict[(y, x, 0, 0, x, 0)] = 50 * x
+        patternDict[(x, 0, 0, 0, x)] = 50 * x
+        patternDict[(y, 0, x, 0, x, 0, y)] = 50 * x
+        patternDict[(y, 0, x, x, 0, 0, y)] = 50 * x
+        patternDict[(y, 0, 0, x, x, 0, y)] = 50 * x
         # dead_2
-        patternDict[(y, x, x, y)] = -5 * x
+        patternDict[(y, x, x, y)] = -50 * x
         x += 2
     return patternDict
-
-# def transform_pattern(patternDict):
-#     new_patternDict = {}
-#     for k,v in patternDict.items():
-#         pattern = []
-#         for i in k:
-#             if i == 0:
-#                 i = '.'
-#             if i == 1:
-#                 i = 'x'
-#             if i == -1:
-#                 i = 'o'
-#             pattern.append(i)
-#         pattern = tuple(pattern)
-#         new_patternDict[pattern] = patternDict[k]
-#     return new_patternDict
-
